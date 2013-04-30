d63574fdb5214bec1c931bee130248b8949abf37
Gustavo de Melo Corrente <gustavomelo-1@hotmail.com>
2013-04-30 18:32:40 -0300

    Alteração de um botão no form
    botao Gustavo Melo

--- MJr-MessageBox/WindowsFormsApplication1/Form1.Designer.cs
+++ MJr-MessageBox/WindowsFormsApplication1/Form1.Designer.cs
@@ -38,6 +38,7 @@
             this.cmbIcone = new System.Windows.Forms.ComboBox();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnEnviar
@@ -116,11 +117,21 @@
             this.label4.TabIndex = 8;
             this.label4.Text = "Icones";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(136, 217);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(472, 265);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.cmbIcone);
@@ -150,6 +161,7 @@
         private System.Windows.Forms.ComboBox cmbIcone;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
     }
 }
 
